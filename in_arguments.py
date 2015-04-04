37425d0be56de0fdd2b00c40e1733c2e7d9df2d3
Leonid Shagabutdinov <leonid@shagabutidnov.com>
2015-04-04 17:13:54 +0600

    context improved

--- in_arguments.py
+++ in_arguments.py
@@ -1,5 +1,6 @@
 import sublime
 import sublime_plugin
+import re
 
 from Context.base import Base
 
@@ -12,6 +13,25 @@ except ImportError:
    "message remove this plugin")
 
 class InArguments(Base):
+
+  def _get_value(self, view, sel):
+    nesting = expression.get_nesting(view, sel.begin(), 2048)
+    if nesting == None:
+      return None
+
+    start, end = nesting[0], nesting[1]
+
+    if sel.begin() != sel.end():
+      nesting_end = expression.get_nesting(view, sel.begin(), 2048)
+      if nesting_end == None:
+        return None
+
+      end_start, end_end = nesting_end[0], nesting_end[1]
+      if end_start != start and end_end != end:
+        return None
+
+    previous_character = view.substr(sublime.Region(start - 2, start - 1))
+    return re.search(r'[\w?!]', previous_character) != None
+
   def on_query_context(self, *args):
-    callback = lambda view, sel: statement.is_arguments(view, sel.b)
-    return self._check_sel('in_arguments', callback, *args)
+    return self._check_sel('in_arguments', self._get_value, *args)
